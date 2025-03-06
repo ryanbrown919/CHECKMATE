bc6994c0d39ac1f075506fdbb86f6e59dc7382e3
felipflop <felipe.canazil@gmail.com>
2025-03-06 13:01:46 -0800

    feat: while true loop

--- hall/main.c
+++ hall/main.c
@@ -7,11 +7,13 @@
 
 int main() {
     gpioInitialise();
-    
-    for (int i = 0; i < 16; i++) {
-        uint32_t current_gray = i ^ (i >> 1);
-        mux_set_pins(current_gray);
-        usleep(10000);
+
+    while (1) {
+        for (int i = 0; i < 16; i++) {
+            uint32_t current_gray = i ^ (i >> 1);
+            mux_set_pins(current_gray);
+            usleep(10000);
+        }
     }
     
     return 0;
