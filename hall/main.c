b480e30aa8fff38c53455f93f0ba7691df7bcbd5
felipflop <felipe.canazil@gmail.com>
2025-03-17 13:11:47 -0700

    feat: proper board print?

--- hall/main.c
+++ hall/main.c
@@ -17,6 +17,13 @@ int main() {
         for (int i = 0; i < 8; i++) {
             printf("%d: %d\n", i, board[i]);
         }
+        // Print the board all in one go
+        for (int r = 0; r < 8; r++) {
+            for (int c = 7; c >= 0; c--) {  // Assuming bit 7 is the leftmost column
+            printf("%d", (board[r] >> c) & 1);
+            }
+            printf("\n");
+        }
         fflush(stdout);
         usleep(10000);
     }
