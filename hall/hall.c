6654522d83c3e6987bcd0e6d909929ddde86fde9
felipflop <felipe.canazil@gmail.com>
2025-03-18 12:25:06 -0700

    fix: proper board print lol

--- hall/hall.c
+++ hall/hall.c
@@ -2,42 +2,23 @@
 #include "mux.h"
 
 const uint32_t hall_to_board_mapping[16][8] = {
-    {7, 2,  2, 3,  2, 7,  5, 4}, /* HALL  7 mapping */
-    {7, 3,  3, 3,  3, 7,  4, 4}, /* HALL  3 mapping */
-    {6, 3,  3, 2,  3, 6,  4, 5}, /* HALL  2 mapping */
-    {6, 2,  2, 2,  2, 6,  5, 5}, /* HALL  6 mapping */
-    {4, 3,  3, 0,  3, 4,  4, 7}, /* HALL  0 mapping */
-    {4, 2,  2, 0,  2, 4,  5, 7}, /* HALL  4 mapping */
-    {5, 2,  2, 1,  2, 5,  5, 6}, /* HALL  5 mapping */
-    {5, 3,  3, 1,  3, 5,  4, 6}, /* HALL  1 mapping */
-    {6, 1,  1, 2,  1, 6,  6, 5}, /* HALL 10 mapping */
-    {7, 1,  1, 3,  1, 7,  6, 4}, /* HALL 11 mapping */
-    {7, 0,  0, 3,  0, 7,  7, 4}, /* HALL 15 mapping */
-    {6, 0,  0, 2,  0, 6,  7, 5}, /* HALL 14 mapping */
-    {4, 1,  1, 0,  1, 4,  6, 7}, /* HALL  8 mapping */
-    {5, 1,  1, 1,  1, 5,  6, 6}, /* HALL  9 mapping */
-    {5, 0,  0, 1,  0, 5,  7, 6}, /* HALL 13 mapping */
-    {4, 0,  0, 0,  0, 4,  7, 7}  /* HALL 12 mapping */
+    {5, 0, 2, 3, 2, 7, 5, 4}, /* HALL  7 mapping */
+    {4, 0, 3, 3, 3, 7, 4, 4}, /* HALL  3 mapping */
+    {4, 1, 3, 2, 3, 6, 4, 5}, /* HALL  2 mapping */
+    {5, 1, 2, 2, 2, 6, 5, 5}, /* HALL  6 mapping */
+    {4, 3, 3, 0, 3, 4, 4, 7}, /* HALL  0 mapping */
+    {5, 3, 2, 0, 2, 4, 5, 7}, /* HALL  4 mapping */
+    {5, 2, 2, 1, 2, 5, 5, 6}, /* HALL  5 mapping */
+    {4, 2, 3, 1, 3, 5, 4, 6}, /* HALL  1 mapping */
+    {6, 1, 1, 2, 1, 6, 6, 5}, /* HALL 10 mapping */
+    {6, 0, 1, 3, 1, 7, 6, 4}, /* HALL 11 mapping */
+    {7, 0, 0, 3, 0, 7, 7, 4}, /* HALL 15 mapping */
+    {7, 1, 0, 2, 0, 6, 7, 5}, /* HALL 14 mapping */
+    {6, 3, 1, 0, 1, 4, 6, 7}, /* HALL  8 mapping */
+    {6, 2, 1, 1, 1, 5, 6, 6}, /* HALL  9 mapping */
+    {7, 2, 0, 1, 0, 5, 7, 6}, /* HALL 13 mapping */
+    {7, 3, 0, 0, 0, 4, 7, 7}  /* HALL 12 mapping */
 };
-
-// const uint32_t hall_to_board_mapping[16][8] = {
-//     {5, 0,  2, 3,  2, 7,  5, 4}, /* HALL  7 mapping */
-//     {4, 0,  3, 3,  3, 7,  4, 4}, /* HALL  3 mapping */
-//     {4, 1,  3, 2,  3, 6,  4, 5}, /* HALL  2 mapping */
-//     {5, 1,  2, 2,  2, 6,  5, 5}, /* HALL  6 mapping */
-//     {3, 4,  3, 0,  3, 4,  4, 7}, /* HALL  0 mapping */
-//     {5, 3,  2, 0,  2, 4,  5, 7}, /* HALL  4 mapping */
-//     {5, 2,  2, 1,  2, 5,  5, 6}, /* HALL  5 mapping */
-//     {4, 2,  3, 1,  3, 5,  4, 6}, /* HALL  1 mapping */
-//     {6, 1,  1, 2,  1, 6,  6, 5}, /* HALL 10 mapping */
-//     {6, 0,  1, 3,  1, 7,  6, 4}, /* HALL 11 mapping */
-//     {7, 0,  0, 3,  0, 7,  7, 4}, /* HALL 15 mapping */
-//     {7, 1,  0, 2,  0, 6,  7, 5}, /* HALL 14 mapping */
-//     {6, 3,  1, 0,  1, 4,  6, 7}, /* HALL  8 mapping */
-//     {6, 2,  1, 1,  1, 5,  6, 6}, /* HALL  9 mapping */
-//     {7, 2,  0, 1,  0, 5,  7, 6}, /* HALL 13 mapping */
-//     {7, 3,  0, 0,  0, 4,  7, 7}  /* HALL 12 mapping */
-// };
 
 void hall_get_squares(uint8_t (*halls)[8]) {
     for (int i = 0; i < 16; i++) {
@@ -64,7 +45,7 @@ void hall_get_squares(uint8_t (*halls)[8]) {
             }
         }
 
-        for (int r = 0; r < 8; r++) {
+        for (int r = 7; r >= 0; r++) {
             if (r == 4) {
                 printf("\n");
             }
