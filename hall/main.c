97df4b6cda1e914ee4479c8c29de9c9acc979a07
felipflop <felipe.canazil@gmail.com>
2025-03-18 11:07:43 -0700

    fix: debug galore

--- hall/main.c
+++ hall/main.c
@@ -14,23 +14,6 @@ int main() {
 
     while (1) {
         hall_get_squares(board);
-    
-        for (int r = 0; r < 8; r++) {
-            if (r == 4) {
-                printf("\n");
-            }
-
-            for (int c = 0; c < 8; c++) {
-                if (c == 4) {
-                    printf(" ");
-                }
-
-                printf("%d ", (board[r] >> (7 - c)) & 1);
-            }
-            printf("\n");
-        }  
-        printf("\n");
-        usleep(2000000);
     }
     
     return 0;
