49acbe8f1ae27320e9033cfb5bab74e122f14808
felipflop <felipe.canazil@gmail.com>
2025-03-18 13:23:50 -0700

    feat: test v6

--- hall/hall.c
+++ hall/hall.c
@@ -2,22 +2,23 @@
 #include "mux.h"
 
 const uint32_t hall_to_board_mapping[16][8] = {
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  7 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  3 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  2 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  6 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  0 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  4 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  5 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  1 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL 10 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL 11 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL 15 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL 14 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  8 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL  9 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}, /* HALL 13 mapping */
-    {0, 1,  2, 3,  4, 5,  6, 7}  /* HALL 12 mapping */
+    /* q3    q      q       q*/
+    {5, 0,  0, 0,  2, 7,  5, 4}, /* HALL  7 mapping */
+    {4, 0,  0, 0,  3, 7,  4, 4}, /* HALL  3 mapping */
+    {4, 1,  0, 0,  3, 6,  4, 5}, /* HALL  2 mapping */
+    {5, 1,  0, 0,  2, 6,  5, 5}, /* HALL  6 mapping */
+    {4, 3,  0, 0,  3, 4,  4, 7}, /* HALL  0 mapping */
+    {5, 3,  0, 0,  2, 4,  5, 7}, /* HALL  4 mapping */
+    {5, 2,  0, 0,  2, 5,  5, 6}, /* HALL  5 mapping */
+    {4, 2,  0, 0,  3, 5,  4, 6}, /* HALL  1 mapping */
+    {6, 1,  0, 0,  1, 6,  6, 5}, /* HALL 10 mapping */
+    {6, 0,  0, 0,  1, 7,  6, 4}, /* HALL 11 mapping */
+    {7, 0,  0, 0,  0, 7,  7, 4}, /* HALL 15 mapping */
+    {7, 1,  0, 0,  0, 6,  7, 5}, /* HALL 14 mapping */
+    {6, 3,  0, 0,  1, 4,  6, 7}, /* HALL  8 mapping */
+    {6, 2,  0, 0,  1, 5,  6, 6}, /* HALL  9 mapping */
+    {7, 2,  0, 0,  0, 5,  7, 6}, /* HALL 13 mapping */
+    {7, 3,  0, 0,  0, 4,  7, 7}  /* HALL 12 mapping */
 };
 
 void hall_get_squares(uint8_t (*halls)[8]) {
