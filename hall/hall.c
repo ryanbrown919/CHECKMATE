44927ee2d722ac417c55f62778d88aaf6de7c702
felipflop <felipe.canazil@gmail.com>
2025-03-18 13:17:01 -0700

    fix: q1

--- hall/hall.c
+++ hall/hall.c
@@ -2,22 +2,22 @@
 #include "mux.h"
 
 const uint32_t hall_to_board_mapping[16][8] = {
-    {5, 0,  2, 3,  2, 7,  5, 4}, /* HALL  7 mapping */
-    {4, 0,  3, 3,  3, 7,  4, 4}, /* HALL  3 mapping */
-    {4, 1,  3, 2,  3, 6,  4, 5}, /* HALL  2 mapping */
-    {5, 1,  2, 2,  2, 6,  5, 5}, /* HALL  6 mapping */
-    {4, 3,  3, 0,  3, 4,  4, 7}, /* HALL  0 mapping */
-    {5, 3,  2, 0,  2, 4,  5, 7}, /* HALL  4 mapping */
-    {5, 2,  2, 1,  2, 5,  5, 6}, /* HALL  5 mapping */
-    {4, 2,  3, 1,  3, 5,  4, 6}, /* HALL  1 mapping */
-    {6, 1,  1, 2,  1, 6,  6, 5}, /* HALL 10 mapping */
-    {6, 0,  1, 3,  1, 7,  6, 4}, /* HALL 11 mapping */
-    {7, 0,  0, 3,  0, 7,  7, 4}, /* HALL 15 mapping */
-    {7, 1,  0, 2,  0, 6,  7, 5}, /* HALL 14 mapping */
-    {6, 3,  1, 0,  1, 4,  6, 7}, /* HALL  8 mapping */
-    {6, 2,  1, 1,  1, 5,  6, 6}, /* HALL  9 mapping */
-    {7, 2,  0, 1,  0, 5,  7, 6}, /* HALL 13 mapping */
-    {7, 3,  0, 0,  0, 4,  7, 7}  /* HALL 12 mapping */
+    {5, 0,  5, 4,  2, 7,  2, 3,}, /* HALL  7 mapping */
+    {4, 0,  4, 4,  3, 7,  3, 3,}, /* HALL  3 mapping */
+    {4, 1,  4, 5,  3, 6,  3, 2,}, /* HALL  2 mapping */
+    {5, 1,  5, 5,  2, 6,  2, 2,}, /* HALL  6 mapping */
+    {4, 3,  4, 7,  3, 4,  3, 0,}, /* HALL  0 mapping */
+    {5, 3,  5, 7,  2, 4,  2, 0,}, /* HALL  4 mapping */
+    {5, 2,  5, 6,  2, 5,  2, 1,}, /* HALL  5 mapping */
+    {4, 2,  4, 6,  3, 5,  3, 1,}, /* HALL  1 mapping */
+    {6, 1,  6, 5,  1, 6,  1, 2,}, /* HALL 10 mapping */
+    {6, 0,  6, 4,  1, 7,  1, 3,}, /* HALL 11 mapping */
+    {7, 0,  7, 4,  0, 7,  0, 3,}, /* HALL 15 mapping */
+    {7, 1,  7, 5,  0, 6,  0, 2,}, /* HALL 14 mapping */
+    {6, 3,  6, 7,  1, 4,  1, 0,}, /* HALL  8 mapping */
+    {6, 2,  6, 6,  1, 5,  1, 1,}, /* HALL  9 mapping */
+    {7, 2,  7, 6,  0, 5,  0, 1,}, /* HALL 13 mapping */
+    {7, 3,  7, 7,  0, 4,  0, 0,}  /* HALL 12 mapping */
 };
 
 void hall_get_squares(uint8_t (*halls)[8]) {
@@ -38,7 +38,7 @@ void hall_get_squares(uint8_t (*halls)[8]) {
                 continue;
             }
 
-            uint32_t bit = (outputs >> 0) & 1;
+            uint32_t bit = (outputs >> j) & 1;
             if (bit) {
                 halls[row][col] = 0;
             } else {
