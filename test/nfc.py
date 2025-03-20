65d1520f5e5bb33ae9fc5bc9201e464ce3bced00
felipflop <felipe.canazil@gmail.com>
2025-03-20 12:37:48 -0700

    feat: spi comm

--- test/nfc.py
+++ test/nfc.py
@@ -1,49 +1,26 @@
 # SPDX-FileCopyrightText: <text> 2015 Tony DiCola, Roberto Laricchia,
 # and Francesco Crisafulli, for Adafruit Industries </text>
-
 # SPDX-License-Identifier: MIT
 
-# Example of detecting and reading a block from a MiFare classic NFC card.
+"""
+Example of detecting and reading a block from a MiFare classic NFC card using SPI.
+This example shows connecting to the PN532 over SPI and writing & reading a MiFare classic RFID tag.
+"""
 
-"""
-This example shows connecting to the PN532 and writing & reading a mifare classic
-type RFID tag
-"""
+import board
 import busio
-
-# Additional import needed for I2C/SPI
-# from digitalio import DigitalInOut
-#
-# NOTE: pick the import that matches the interface being used
-#
+from digitalio import DigitalInOut
 from adafruit_pn532.adafruit_pn532 import MIFARE_CMD_AUTH_B
-from adafruit_pn532.i2c import PN532_I2C
-
-# from adafruit_pn532.spi import PN532_SPI
-# from adafruit_pn532.uart import PN532_UART
-
-# I2C connection:
-i2c = busio.I2C(3, 2)
-
-# Non-hardware reset/request with I2C
-pn532 = PN532_I2C(i2c, debug=False)
-
-# With I2C, we recommend connecting RSTPD_N (reset) to a digital pin for manual
-# harware reset
-# reset_pin = DigitalInOut(board.D6)
-# On Raspberry Pi, you must also connect a pin to P32 "H_Request" for hardware
-# wakeup! this means we don't need to do the I2C clock-stretch thing
-# req_pin = DigitalInOut(board.D12)
-# pn532 = PN532_I2C(i2c, debug=False, reset=reset_pin, req=req_pin)
+from adafruit_pn532.spi import PN532_SPI
 
 # SPI connection:
-# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-# cs_pin = DigitalInOut(board.D5)
-# pn532 = PN532_SPI(spi, cs_pin, debug=False)
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+cs_pin = DigitalInOut(board.D5)
+pn532 = PN532_SPI(spi, cs_pin, debug=False)
 
-# UART connection
-# uart = busio.UART(board.TX, board.RX, baudrate=115200, timeout=0.1)
-# pn532 = PN532_UART(uart, debug=False)
+# Optionally, perform a hardware reset if needed:
+# reset_pin = DigitalInOut(board.D6)
+# pn532.hard_reset(reset_pin)
 
 ic, ver, rev, support = pn532.firmware_version
 print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
@@ -56,30 +33,28 @@ print("Waiting for RFID/NFC card to write to!")
 key = b"\xFF\xFF\xFF\xFF\xFF\xFF"
 
 while True:
-    # Check if a card is available to read
+    # Check if a card is available to read, timeout set to 0.5 seconds.
     uid = pn532.read_passive_target(timeout=0.5)
     print(".", end="")
-    # Try again if no card is available.
+    # Break out of loop if a card is detected.
     if uid is not None:
         break
 
 print("")
-
 print("Found card with UID:", [hex(i) for i in uid])
 print("Authenticating block 4 ...")
 
 authenticated = pn532.mifare_classic_authenticate_block(uid, 4, MIFARE_CMD_AUTH_B, key)
 if not authenticated:
     print("Authentication failed!")
-
-# Set 16 bytes of block to 0xFEEDBEEF
-data = bytearray(16)
-data[0:16] = b"\xFE\xED\xBE\xEF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-
-# Write 16 byte block.
-pn532.mifare_classic_write_block(4, data)
-# Read block #6
-print(
-    "Wrote to block 4, now trying to read that data:",
-    [hex(x) for x in pn532.mifare_classic_read_block(4)],
-)
+else:
+    # Prepare 16 bytes of data: set to 0xFEEDBEEF and pad with zeros.
+    data = bytearray(16)
+    data[0:16] = b"\xFE\xED\xBE\xEF\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    
+    # Write the data block.
+    pn532.mifare_classic_write_block(4, data)
+    
+    # Read back block 4.
+    read_data = pn532.mifare_classic_read_block(4)
+    print("Wrote to block 4, now trying to read that data:", [hex(x) for x in read_data])
