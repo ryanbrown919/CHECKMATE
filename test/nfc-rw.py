49199e081babdee62dc291b61ab9341659d616b3
felipflop <felipe.canazil@gmail.com>
2025-03-21 11:23:10 -0700

    feat: superloop

--- test/nfc-rw.py
+++ test/nfc-rw.py
@@ -10,21 +10,35 @@ def setup()
 def main():
     while True:
         mode = input("Enter mode (read, write): ")
-        
-        if mode == "w":
-            piece = input("Enter piece: ")
+        if mode == "w" or mode == "write":
+            while True:
+                piece = input("Enter piece: ")
+                if piece in ["K", "Q", "R", "B", "N", "P", "k", "q", "r", "b", "n", "p"]:
+                    break
+                else:
+                    print("Invalid piece. Please try again.")
+
             write_ok = nfc.write(piece)
             if write_ok:
                 print("Write successful")
             else:
                 print("Write failed")
 
-        elif mode == "r":
+        elif mode == "r" or "read":
             tag_present, fen_piece = nfc.read()
+
             if tag_present:
                 print("Tag present, piece: ", fen_piece)
             else:
                 print("No tag present")
+        
+        else:
+            print("Invalid mode")
 
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        main()
 
         
