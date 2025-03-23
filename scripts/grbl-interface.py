17940f0fd296ce956e716bc137920809e6c3aadd
felipflop <felipe.canazil@gmail.com>
2025-03-22 22:20:24 -0700

    feat: debug

--- scripts/grbl-interface.py
+++ scripts/grbl-interface.py
@@ -18,8 +18,10 @@ grbl.cnect("/dev/ttyACM0", 115200)
 # Start polling GRBL state
 grbl.poll_start()
 
-# Set relative positioning mode (G91) and send newline
-grbl.send_immediately("G91\n")
+# Set relative positioning mode (G91)
+cmd = "G91\n"
+print("Sending command:", repr(cmd))
+grbl.send_immediately(cmd)
 
 print("Use W/A/S/D to move Up/Left/Down/Right by 50mm. Press Q to quit.")
 
@@ -27,20 +29,29 @@ try:
     while True:
         command = input("Direction (W/A/S/D/Q): ").strip().lower()
         if command == "w":
-            # Ensure there are spaces between command parts
-            grbl.send_immediately("G1 Y{} F1000\n".format(STEP_SIZE))
+            cmd = "G1 " + "Y" + str(STEP_SIZE) + " " + "F1000\n"
+            print("Sending command:", repr(cmd))
+            grbl.send_immediately(cmd)
         elif command == "s":
-            grbl.send_immediately("G1 Y{} F1000\n".format(-STEP_SIZE))
+            cmd = "G1 " + "Y" + str(-STEP_SIZE) + " " + "F1000\n"
+            print("Sending command:", repr(cmd))
+            grbl.send_immediately(cmd)
         elif command == "a":
-            grbl.send_immediately("G1 X{} F1000\n".format(-STEP_SIZE))
+            cmd = "G1 " + "X" + str(-STEP_SIZE) + " " + "F1000\n"
+            print("Sending command:", repr(cmd))
+            grbl.send_immediately(cmd)
         elif command == "d":
-            grbl.send_immediately("G1 X{} F1000\n".format(STEP_SIZE))
+            cmd = "G1 " + "X" + str(STEP_SIZE) + " " + "F1000\n"
+            print("Sending command:", repr(cmd))
+            grbl.send_immediately(cmd)
         elif command == "q":
             print("Exiting.")
             break
         else:
             print("Invalid input. Use W/A/S/D/Q.")
 finally:
-    # Return to absolute positioning (G90) and disconnect
-    grbl.send_immediately("G90\n")
+    # Return to absolute positioning (G90)
+    cmd = "G90\n"
+    print("Sending command:", repr(cmd))
+    grbl.send_immediately(cmd)
     grbl.disconnect()
