52e6503cdf33e29988d4dde4802727994dfa1326
felipflop <felipe.canazil@gmail.com>
2025-03-28 18:29:26 -0700

    feat: mux remaps

--- firmware/inc/mux.h
+++ firmware/inc/mux.h
@@ -7,12 +7,12 @@
 #define MUX_S0 6
 #define MUX_S1 13
 #define MUX_S2 19
-#define MUX_S3 5
+#define MUX_S3 26
 
-#define MUX_Y_1 26   
-#define MUX_Y_2 16   
-#define MUX_Y_3 20   
-#define MUX_Y_4 21
+#define MUX_Y_1 21   
+#define MUX_Y_2 12   
+#define MUX_Y_3 26   
+#define MUX_Y_4 20
 
 void mux_init(void);
 void mux_set_pins(char nibble);
