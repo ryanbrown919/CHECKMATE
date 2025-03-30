24b0a56c21cb985046316c192f93e21036c48b0b
Ryan Brown <ryanbrown919@gmail.com>
2025-03-29 19:48:18 -0700

    fix maybe

--- scripts/controls/control_system.py
+++ scripts/controls/control_system.py
@@ -220,7 +220,7 @@ class ChessControlSystem:
         self.update_ui()
         """Call all registered observer callbacks with the updated board."""
         for callback in self.observers:
-            Clock.schedule_once(lambda dt, cb=callback: cb(self.board, self.game_state), 0.1)
+            Clock.schedule_once(lambda dt, cb=callback: cb(self.board), 0.1)
             #callback(self.board)
 
     # def start_game(self):
