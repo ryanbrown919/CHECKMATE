65137b496cc8248c421ce78c14cf356ffab7fffe
felipflop <felipe.canazil@gmail.com>
2025-03-31 09:50:59 -0700

    feat: switch

--- scripts/servo.py
+++ scripts/servo.py
@@ -2,18 +2,12 @@ import lgpio
 import sys
 import time
 
-'''
-@ryan: install pigpio (pip install pigpio) and run the daemon "sudo pigpiod" in the terminal whenever power cycling the pi so that this can work"
-'''
-
-# Configuration
-SERVO_GPIO_PIN = 17    # Change this to the GPIO pin you are using for the servo
-DUTY_CYCLE = 50        # Duty cycle for the servo (0-100)
-
-class Servo():
+class Rocker():
     def __init__(self):
+        self.servo_pin = 17
+        self.switch_pin = None # Placeholder for switch pin
         self.handle = lgpio.gpiochip_open(0)
-        lpgio.gpio_claim_output(self.handle, SERVO_GPIO_PIN)
+        lpgio.gpio_claim_output(self.handle, self.servo_pin)
         self.state = "home"
         self.last_state = "home"  
 
@@ -21,42 +15,46 @@ class Servo():
         self.open()
         time.sleep(0.5)
         self.home()
-        self.state = "home"
-        self.last_state = "open"
+        self.state = get_switch_state()
+
+    def get_switch_state(self):
+        return lgpio.gpio_read(self.handle, self.switch_pin)
 
     def home(self):
-        lgpio.tx_pwm(self.handle, SERVO_GPIO_PIN, 10000, 50)
+        lgpio.tx_pwm(self.handle, self.servo_pin, 10000, 50)
         self.state = "home"
     
     def open(self):
-        lgpio.tx_pwm(self.handle, SERVO_GPIO_PIN, 10000, 62)
-        self.state = "open"
+        initial_state = self.get_switch_state()
+        while not (self.get_switch_state() ^ initial_state):
+            lgpio.tx_pwm(self.handle, self.servo_pin, 10000, 62)
+            time.sleep(0.005)
+        self.home()
     
     def close(self):
-        lgpio.tx_pwm(self.handle, SERVO_GPIO_PIN, 10000, 38)
-        self.state = "close"
+        initial_state = self.get_switch_state()
+        while not (self.get_switch_state() ^ initial_state):
+            lgpio.tx_pwm(self.handle, self.servo_pin, 10000, 38)
+            time.sleep(0.005)
+        self.home()
     
     def toggle(self):
         if self.last_state == "open":
-            self.close()
-            time.sleep(0.5)
-            self.home()
-            self.last_state = "close"
-        elif self.last_state == "close":
-            self.open()
-            time.sleep(0.5)
-            self.home()
-            self.last_state = "open"
+            if self.get_switch_state():
+                self.close()
+            else:
+                self.open()
 
 if __name__ == "__main__":
-    servo = Servo()
-    servo.begin()
+    rocker = Rocker()
+    rocker.begin()
     try:
         while True:
             input("Press Enter to toggle the servo")
-            servo.toggle()
+            rocker.toggle()
     except KeyboardInterrupt:
-        servo.pi.stop()
+        rocker.home()
+        rocker.handle.cleanup()
         sys.exit(0)
 
 
