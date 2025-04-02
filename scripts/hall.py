39285e4663f1df03894ffe56126b1602a95a91f0
felipflop <felipe.canazil@gmail.com>
2025-04-02 12:26:37 -0700

    fix: revert mapping

--- scripts/hall.py
+++ scripts/hall.py
@@ -48,25 +48,23 @@ class SenseLayer:
     # Mapping from hall effect sensors to board coordinates
     hall_to_board_mapping = [
         # q1    q2     q3     q4
-        [[3, 5],  [0, 2],  [4, 2],  [7, 5]], # HALL  7 mapping 
-        [[3, 4],  [0, 3],  [4, 3],  [7, 4]], # HALL  3 mapping 
-        [[2, 4],  [1, 3],  [5, 3],  [6, 4]], # HALL  2 mapping 
-        [[2, 5],  [1, 2],  [5, 2],  [6, 5]], # HALL  6 mapping 
-        [[0, 4],  [3, 3],  [7, 3],  [4, 4]], # HALL  0 mapping 
-        [[0, 5],  [3, 2],  [7, 2],  [4, 5]], # HALL  4 mapping 
-        [[1, 5],  [2, 2],  [6, 2],  [5, 5]], # HALL  5 mapping 
-        [[1, 4],  [2, 3],  [6, 3],  [5, 4]], # HALL  1 mapping 
-        [[2, 6],  [1, 1],  [5, 1],  [6, 6]], # HALL 10 mapping 
-        [[3, 6],  [0, 1],  [4, 1],  [7, 6]], # HALL 11 mapping 
-        [[3, 7],  [0, 0],  [4, 0],  [7, 7]], # HALL 15 mapping 
-        [[2, 7],  [1, 0],  [5, 0],  [6, 7]], # HALL 14 mapping 
-        [[0, 6],  [3, 1],  [7, 1],  [4, 6]], # HALL  8 mapping 
-        [[1, 6],  [2, 1],  [6, 1],  [5, 6]], # HALL  9 mapping 
-        [[1, 7],  [2, 0],  [6, 0],  [5, 7]], # HALL 13 mapping 
-        [[0, 7],  [3, 0],  [7, 0],  [4, 7]]  # HALL 12 mapping 
+        [[2, 7], [2, 3], [5, 0], [5, 4]],  # HALL  7 mapping
+        [[3, 7], [3, 3], [4, 0], [4, 4]],  # HALL  3 mapping
+        [[3, 6], [3, 2], [4, 1], [4, 5]],  # HALL  2 mapping
+        [[2, 6], [2, 2], [5, 1], [5, 5]],  # HALL  6 mapping
+        [[3, 4], [3, 0], [4, 3], [4, 7]],  # HALL  0 mapping
+        [[2, 4], [2, 0], [5, 3], [5, 7]],  # HALL  4 mapping
+        [[2, 5], [2, 1], [5, 2], [5, 6]],  # HALL  5 mapping
+        [[3, 5], [3, 1], [4, 2], [4, 6]],  # HALL  1 mapping
+        [[1, 6], [1, 2], [6, 1], [6, 5]],  # HALL 10 mapping
+        [[1, 7], [1, 3], [6, 0], [6, 4]],  # HALL 11 mapping
+        [[0, 7], [0, 3], [7, 0], [7, 4]],  # HALL 15 mapping
+        [[0, 6], [0, 2], [7, 1], [7, 5]],  # HALL 14 mapping
+        [[1, 4], [1, 0], [6, 3], [6, 7]],  # HALL  8 mapping
+        [[1, 5], [1, 1], [6, 2], [6, 6]],  # HALL  9 mapping
+        [[0, 5], [0, 1], [7, 2], [7, 6]],  # HALL 13 mapping
+        [[0, 4], [0, 0], [7, 3], [7, 7]]   # HALL 12 mapping
     ]
-
-    
 
     # Chess notation mapping
     square_mapping = {f"{file}{rank}": (ord(file) - ord('a'), int(rank) - 1)
