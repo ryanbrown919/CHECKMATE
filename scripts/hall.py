27f7408cf85226bddfe93fc0a51c58e8492fd31a
felipflop <felipe.canazil@gmail.com>
2025-04-02 12:30:35 -0700

    feat: transpose

--- scripts/hall.py
+++ scripts/hall.py
@@ -48,22 +48,22 @@ class SenseLayer:
     # Mapping from hall effect sensors to board coordinates
     hall_to_board_mapping = [
         # q1    q2     q3     q4
-        [[2, 7], [2, 3], [5, 0], [5, 4]],  # HALL  7 mapping
-        [[3, 7], [3, 3], [4, 0], [4, 4]],  # HALL  3 mapping
-        [[3, 6], [3, 2], [4, 1], [4, 5]],  # HALL  2 mapping
-        [[2, 6], [2, 2], [5, 1], [5, 5]],  # HALL  6 mapping
-        [[3, 4], [3, 0], [4, 3], [4, 7]],  # HALL  0 mapping
-        [[2, 4], [2, 0], [5, 3], [5, 7]],  # HALL  4 mapping
-        [[2, 5], [2, 1], [5, 2], [5, 6]],  # HALL  5 mapping
-        [[3, 5], [3, 1], [4, 2], [4, 6]],  # HALL  1 mapping
-        [[1, 6], [1, 2], [6, 1], [6, 5]],  # HALL 10 mapping
-        [[1, 7], [1, 3], [6, 0], [6, 4]],  # HALL 11 mapping
-        [[0, 7], [0, 3], [7, 0], [7, 4]],  # HALL 15 mapping
-        [[0, 6], [0, 2], [7, 1], [7, 5]],  # HALL 14 mapping
-        [[1, 4], [1, 0], [6, 3], [6, 7]],  # HALL  8 mapping
-        [[1, 5], [1, 1], [6, 2], [6, 6]],  # HALL  9 mapping
-        [[0, 5], [0, 1], [7, 2], [7, 6]],  # HALL 13 mapping
-        [[0, 4], [0, 0], [7, 3], [7, 7]]   # HALL 12 mapping
+        [[7, 2], [3, 2], [0, 5], [4, 5]],  # HALL  7 mapping
+        [[7, 3], [3, 3], [0, 4], [4, 4]],  # HALL  3 mapping
+        [[6, 3], [2, 3], [1, 4], [5, 4]],  # HALL  2 mapping
+        [[6, 2], [2, 2], [1, 5], [5, 5]],  # HALL  6 mapping
+        [[4, 3], [0, 3], [3, 4], [7, 4]],  # HALL  0 mapping
+        [[4, 2], [0, 2], [3, 5], [7, 5]],  # HALL  4 mapping
+        [[5, 2], [1, 2], [2, 5], [6, 5]],  # HALL  5 mapping
+        [[5, 3], [1, 3], [2, 4], [6, 4]],  # HALL  1 mapping
+        [[6, 1], [2, 1], [1, 6], [5, 6]],  # HALL 10 mapping
+        [[7, 1], [3, 1], [0, 6], [4, 6]],  # HALL 11 mapping
+        [[7, 0], [3, 0], [0, 7], [4, 7]],  # HALL 15 mapping
+        [[6, 0], [2, 0], [1, 7], [5, 7]],  # HALL 14 mapping
+        [[4, 1], [0, 1], [3, 6], [7, 6]],  # HALL  8 mapping
+        [[5, 1], [1, 1], [2, 6], [6, 6]],  # HALL  9 mapping
+        [[5, 0], [1, 0], [2, 7], [6, 7]],  # HALL 13 mapping
+        [[4, 0], [0, 0], [3, 7], [7, 7]]   # HALL 12 mapping
     ]
 
     # Chess notation mapping
