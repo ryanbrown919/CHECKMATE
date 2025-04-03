4fd03445dbaa139ee20d3ba06efd872f72ce90c2
felipflop <felipe.canazil@gmail.com>
2025-04-02 21:47:05 -0700

    feat: thread

--- scripts/controls/rocker_control.py
+++ scripts/controls/rocker_control.py
@@ -1,5 +1,6 @@
 import lgpio
 import time
+import threading
 
 class Rocker():
     def __init__(self):
@@ -15,6 +16,29 @@ class Rocker():
         self.OPEN_DUTY = 8.5
         self.CLOSE_DUTY = 6.5
         self.MAX_WAIT_TIME = 2.0
+        
+        # Threading related attributes
+        self.pwm_thread = None
+        self.thread_lock = threading.Lock()
+        self.running = False
+        self.current_duty = 0
+        self.start_pwm_thread()
+
+    def start_pwm_thread(self):
+        """Start the PWM control thread"""
+        if self.pwm_thread is None or not self.pwm_thread.is_alive():
+            self.running = True
+            self.pwm_thread = threading.Thread(target=self._pwm_control_loop)
+            self.pwm_thread.daemon = True  # Thread will close when main program exits
+            self.pwm_thread.start()
+    
+    def _pwm_control_loop(self):
+        """Main PWM control loop that runs in the thread"""
+        while self.running:
+            with self.thread_lock:
+                if self.current_duty > 0:
+                    lgpio.tx_pwm(self.handle, self.servo_pin, self.PWM_FREQ, self.current_duty)
+            time.sleep(0.02)  # Small delay to prevent CPU hogging
 
     def begin(self):
         if self.get_switch_state():
@@ -52,7 +76,9 @@ class Rocker():
         self.home()
     
     def _move_servo(self, duty_cycle):
-        lgpio.tx_pwm(self.handle, self.servo_pin, self.PWM_FREQ, duty_cycle)
+        """Thread-safe way to update the duty cycle"""
+        with self.thread_lock:
+            self.current_duty = duty_cycle
     
     def _wait_for_switch_change(self):
         initial_state = self.get_switch_state()
@@ -63,7 +89,9 @@ class Rocker():
 
     def _stop_servo(self):
         """Stop sending PWM signals to reduce jitter"""
-        lgpio.tx_pwm(self.handle, self.servo_pin, 0, 0)
+        with self.thread_lock:
+            self.current_duty = 0
+            lgpio.tx_pwm(self.handle, self.servo_pin, 0, 0)
     
     def toggle(self):
         if not self.get_switch_state():
@@ -72,6 +100,10 @@ class Rocker():
             self.to_black()
 
     def cleanup(self):
+        """Clean up resources including the PWM thread"""
+        self.running = False
+        if self.pwm_thread and self.pwm_thread.is_alive():
+            self.pwm_thread.join(1.0)  # Wait up to 1 second for thread to finish
         lgpio.tx_pwm(self.handle, self.servo_pin, 0, 0)
         lgpio.gpiochip_close(self.handle)
 
