41827d3d1a95a2cb50b02f2fb3d22d941f2327bb
felipflop <felipe.canazil@gmail.com>
2025-04-03 00:36:52 -0700

    feat: rm print again

--- scripts/controls/gantry_control.py
+++ scripts/controls/gantry_control.py
@@ -1252,7 +1252,6 @@ class GantryControl:
                 while not self.finished:
                     self.ser.write(b'?')
                     status = self.ser.readline().decode().strip()
-                    print(f"Status: {status}")
                     if '<Idle' in status:
                         self.finished = True
                         self.ser.flushInput()
