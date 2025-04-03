10a212c2353f0922306e22ae21f6697d2b6881d3
felipflop <felipe.canazil@gmail.com>
2025-04-02 18:27:20 -0700

    fix: deprecate

--- scripts/carlsen.py
+++ scripts/carlsen.py
@@ -3,8 +3,9 @@ from gantry import Gantry
 
 def toggle_magnet(gantry):
     gantry.send("M8")
-    time.sleep(0.2)
+    time.sleep(0.5)
     gantry.send("M9")
+    time.sleep(0.5)
 
 def carlsen(gantry):
     for i in range(8):
@@ -14,12 +15,10 @@ def carlsen(gantry):
     
     for i in range(7, -1, -1):
         gantry.move(50, i*50, blocking=True)
-        gantry.send_commands(cmd)
         toggle_magnet(gantry)
 
     for i in range(8):
         gantry.move(300, i*50, blocking=True)
-        gantry.send_commands(cmd)
         toggle_magnet(gantry)
     
     for i in range(7, -1, -1):
